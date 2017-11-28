06dcac6e164183b7a2183fe5d6c4070fc347850b
wangyingying <729167844@.com>
2017-11-28 17:50:16 +0800

    实现新闻列表页的布局

--- src/components/newlist/newlist.vue
+++ src/components/newlist/newlist.vue
@@ -1,12 +1,65 @@
 <template>
-  <div>
-      <h3>我就是新闻列表的组件</h3>
+  <div class="newlist">
+      <div class="newlistInfo">
+          <img src="http://vue.studyit.io/upload/201504/16/201504161149414479.jpg" alt="">
+          <div>
+              <h1 class="title">1季度多家房企利润跌幅超50% 去库存促销战打响</h1>
+              <p>
+                  <span>发表时间：2015-04-16 11:50:28</span>
+                  <span>点击：1次</span>
+              </p>
+          </div>
+      </div>
+       <div class="newlistInfo">
+          <img src="http://vue.studyit.io/upload/201504/16/201504161149414479.jpg" alt="">
+          <div>
+              <h1 class="title">1季度多家房企利润跌幅超50% 去库存促销战打响</h1>
+              <p>
+                  <span>发表时间：2015-04-16 11:50:28</span>
+                  <span>点击：1次</span>
+              </p>
+          </div>
+      </div>
+       <div class="newlistInfo">
+          <img src="http://vue.studyit.io/upload/201504/16/201504161149414479.jpg" alt="">
+          <div>
+              <h1 class="title">1季度多家房企利润跌幅超50% 去库存促销战打响</h1>
+              <p>
+                  <span>发表时间：2015-04-16 11:50:28</span>
+                  <span>点击：1次</span>
+              </p>
+          </div>
+      </div>
   </div>
 </template>
 <script>
     
 </script>
 <style lang="scss" scoped>
-
+.newlist{
+    .newlistInfo{
+        display: flex;
+        justify-content: space-between;
+        padding: 5px;
+        img{
+            width: 60px;
+            height: 60px;
+        }
+        div{
+            padding-left: 10px;
+            h1{
+                font-size: 14px;
+            }
+            p{
+                display: flex;
+                justify-content: space-between;
+                span{
+                    font-size:13px;
+                    color:darkgrey;
+                }
+            }
+        }
+    }
+}
 </style>
 
