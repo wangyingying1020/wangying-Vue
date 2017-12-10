ce7923281f6b2ac9684bc794ea63b47ad442d7c3
wangyingying <729167844@.com>
2017-12-10 21:16:25 +0800

    完成评论的加载更多和发表评论

--- src/components/common/comment.vue
+++ src/components/common/comment.vue
@@ -3,9 +3,9 @@
       <h1>发表评论</h1>
       <hr>
       <!-- 评论输入框 -->
-      <textarea placeholder="这是一个吐槽区域，请尽情发挥...." maxlength="120" rows="5"></textarea>
+      <textarea placeholder="这是一个吐槽区域，请尽情发挥...." maxlength="120" rows="5" v-model="msg"></textarea>
       <!-- 发表评论按钮 -->
-      <mt-button type="primary" size="large">发表评论</mt-button>
+      <mt-button type="primary" size="large" @click="addComment">发表评论</mt-button>
       <!-- 评论详情 -->
       <div class="commentContent">
           <div v-for="(item,index) in commentInfo" :key="item.add_time">
@@ -18,16 +18,18 @@
           </div>
       </div>
       <!-- 加载更多 -->
-      <mt-button type="primary" size="large" plain>加载更多</mt-button>
+      <mt-button type="primary" size="large" plain @click="getContents">加载更多</mt-button>
 
   </div>
 </template>
 <script>
+import { Toast } from "mint-ui";
     export default{
         data(){
             return{
-                pageindex:1,
-                commentInfo:[]
+                pageindex:1,//页数
+                commentInfo:[],//存放评论消息详情
+                msg:"",//评论消息
             }
         },
         created(){
@@ -37,10 +39,38 @@
             getContent(){
                 this.$http.get("api/getcomments/"+this.id+"?pageindex="+this.pageindex).then(res=>{
                     // console.log(res.body);
-                    this.commentInfo=res.body.message;
+                    if(res.body.status==0){
+                        this.commentInfo=this.commentInfo.concat(res.body.message);
+                    }else{
+                        Toast("获取评论失败");
+                    }
                     // console.log(this.commentInfo);
                 })
+            },
+            getContents(){
+                this.pageindex++;
+                this.getContent();
+            },
+            addComment(){
+                if(!this.msg.trim().length==0){
+                    this.$http.post("api/postcomment/"+this.id,{content:this.msg}).then(res=>{
+                        console.log(res.body);
+                        console.log(Date.now());
+                        if(res.body.status==0){
+                            
+                            this.commentInfo.unshift({
+                                user_name:"匿名用户",
+                                add_time:Date.now(),
+                                content:this.msg
+                            })
+                            this.msg="";
+                        }
+                    })
+                }else{
+                    Toast("评论不能为空")
+                }
             }
+
         },
         props:["id"]
     }
